d14662f9ca3f490efdbe2b7cf53889b8fd166878
Noah Hay <noahjohnhay@gmail.com>
2024-10-08 11:47:57 -0300

    Delete jnlp-docker-agent directory

--- jnlp-docker-agent/alpine/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-AZURL="http://169.254.169.254/latest/meta-data/placement/availability-zone"
-export AWS_DEFAULT_REGION=`curl --connect-timeout 0.5 -s $AZURL | sed 's/[a-z]$//'`
-
-exec /usr/local/bin/jenkins-agent "$@"
